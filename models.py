9df76e69619b6b4306902df7bd78b39f67c9a8cb
deep-priyo <priyadeepmullick@gmail.com>
2025-12-18 11:58:03 +0530

    added default value for timestamp

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
     message_body = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     urgency_score = db.Column(db.Float)
     status = db.Column(db.String(20), default="open")
 
