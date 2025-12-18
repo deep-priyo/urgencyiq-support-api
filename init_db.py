5cdc54f6d9926b7234ede50f7498e7927619bbf6
deep-priyo <priyadeepmullick@gmail.com>
2025-12-18 11:57:50 +0530

    database intializer for deployment

---
+++ init_db.py
@@ -0,0 +1,7 @@
+from app import app, db
+
+if __name__ == "__main__":
+    with app.app_context():
+        print("Creating database tables...")
+        db.create_all()
+        print("Database tables created successfully!")
